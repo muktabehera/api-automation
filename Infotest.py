4f3f00625443f8de5aadae779bb07bd5e5aef4c4
muktabehera <mukta.behera@gmail.com>
2017-08-17 21:58:33 -0700

    first commit

--- Infotest.py
+++ Infotest.py
@@ -1,12 +1,6 @@
-print("What is swagger?")
-print("The World's Most Popular API Tooling. Swagger is a specification and complete framework implementation for describing, producing, consuming, and visualizing RESTful web services.")
-print("https://github.com/digium/swagger-py")
-print("Swagger.py is a Python library for using Swagger defined API's")
-
 #import webbrowser
 #webbrowser.get(using="C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s").open("http://172.30.4.216/content/apidocs/index.html")
 #webbrowser.open('http://172.30.4.216/content/apidocs/index.html')
-
 from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("http://172.30.4.216/content/apidocs/index.html")
---
+++ test.py
@@ -0,0 +1,4 @@
+print("What is swagger?")
+print("The World's Most Popular API Tooling. Swagger is a specification and complete framework implementation for describing, producing, consuming, and visualizing RESTful web services.")
+print("https://github.com/digium/swagger-py")
+print("Swagger.py is a Python library for using Swagger defined API's")
